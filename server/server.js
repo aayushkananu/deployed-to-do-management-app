4925e3f0310aa5db8f36304834609520c5da7c35
aayushkananu <aayushka.budhathoki@bison.howard.edu>
2024-04-21 19:49:50 -0400

    bugs resolved

--- server/server.js
+++ server/server.js
@@ -1,11 +1,14 @@
 const PORT = process.env.PORT ?? 8000
 const express = require('express')
+const cors = require('cors')
 const app = express()
 const pool = require('./db')    
 
+app.use(cors())
 // get all tasks
 app.get('/todos', async(req, res) => {
-const userEmail = "aayushka@testing.com"
+    console.log(req)
+    const userEmail = req.params
 
     try{
         const todos = await pool.query('SELECT * FROM todos WHERE user_email = $1', [userEmail]);
